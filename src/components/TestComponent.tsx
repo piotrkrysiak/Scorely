f98847fbf9675ee65d660a189114d1aa266b03e1
piotrkrysiak <plotrekhqpl@gmail.com>
2021-10-25 19:10:28 +0200

    Basic tests config with testing-library

---
+++ src/components/TestComponent.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const TestComponent = () => (
+  <View>
+    <Text>title</Text>
+  </View>
+);
+
+export default TestComponent;
