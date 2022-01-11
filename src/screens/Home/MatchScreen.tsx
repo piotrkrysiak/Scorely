b44c807d24698770465d31e4dd30fb02735e5246
piotrkrysiak <plotrekhqpl@gmail.com>
2022-01-12 00:03:21 +0100

    S-039 WebView with match details added

--- src/screens/Home/MatchScreen.tsx
+++ src/screens/Home/MatchScreen.tsx
@@ -1,5 +1,7 @@
+import { API_KEY } from '@env';
 import React, { FC } from 'react';
-import { Text, Route, SafeAreaView } from 'react-native';
+import { Route, SafeAreaView } from 'react-native';
+import WebView from 'react-native-webview';
 
 interface Props {
   route: Route;
@@ -7,9 +9,30 @@ interface Props {
 
 const MatchScreen: FC<Props> = ({ route }) => {
   const { id } = route.params;
+
+  const widget = `<div id="wg-api-football-fixture"
+      data-host="v3.football.api-sports.io"
+      data-refresh="60"
+      data-id=${id}
+      data-key=${API_KEY}
+      data-theme="dark"
+      data-show-errors="true"
+    class="api_football_loader"
+    >
+  </div>
+  <script
+    type="module"
+    src="https://widgets.api-sports.io/football/1.1.8/widget.js">
+  </script>`;
+
   return (
-    <SafeAreaView>
-      <Text>{id}</Text>
+    <SafeAreaView style={{ flex: 1 }}>
+      <WebView
+        scalesPageToFit
+        source={{
+          html: widget,
+        }}
+      />
     </SafeAreaView>
   );
 };
